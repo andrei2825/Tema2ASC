b897913f1270ac7e18c57853f0f37f45977709d1
andrei.porumb <andrei.porumb@nehalem-wn22.grid.pub.ro>
2022-04-30 23:11:19 +0300

    final

--- solver_blas.c
+++ solver_blas.c
@@ -4,33 +4,6 @@
  */
 #include "utils.h"
 #include "cblas.h"
-// func cblas_dgemm(_ __Order: CBLAS_ORDER, 
-//                _ __TransA: CBLAS_TRANSPOSE, 
-//                _ __TransB: CBLAS_TRANSPOSE, 
-//                _ __M: Int32, 
-//                _ __N: Int32, 
-//                _ __K: Int32, 
-//                _ __alpha: Double, 
-//                _ __A: UnsafePointer<Double>!, 
-//                _ __lda: Int32, 
-//                _ __B: UnsafePointer<Double>!, 
-//                _ __ldb: Int32, 
-//                _ __beta: Double, 
-//                _ __C: UnsafeMutablePointer<Double>!, 
-//                _ __ldc: Int32)
-
-// func cblas_dtrmm(_ __Order: CBLAS_ORDER, 
-//                _ __Side: CBLAS_SIDE, 
-//                _ __Uplo: CBLAS_UPLO, 
-//                _ __TransA: CBLAS_TRANSPOSE, 
-//                _ __Diag: CBLAS_DIAG, 
-//                _ __M: Int32, 
-//                _ __N: Int32, 
-//                _ __alpha: Double, 
-//                _ __A: UnsafePointer<Double>!, 
-//                _ __lda: Int32, 
-//                _ __B: UnsafeMutablePointer<Double>!, 
-//                _ __ldb: Int32)
 /* 
  * Add your BLAS implementation here
  */
